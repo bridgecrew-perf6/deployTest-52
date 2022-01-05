b66a0c9e3670ce6d76189a18bf99b6cff39221c0
wls0 <kjy0836@naver.com>
2022-01-05 13:13:26 +0900

    down test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+const data = require('../data/test.json')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express release123' });
 });
-
+router.get('/1', function(req, res, next) {
+  console.log(typeof(data))
+  res.json(data)
+});
 module.exports = router;
