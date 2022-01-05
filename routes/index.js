0d53bf319d91ae6a3d6655f34b616e58ebf0c551
wls0 <kjy0836@naver.com>
2022-01-05 15:48:05 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 const data = require('../data/test1.json')
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express release123' });
+  res.render('index', { title: 'Express release1233312312' });
 });
 router.get('/1', function(req, res, next) {
   console.log(typeof(data))
