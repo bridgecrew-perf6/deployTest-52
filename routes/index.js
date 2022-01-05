542bd11104bb8f8063afe69dc5906530b6a326b1
wls0 <kjy0836@naver.com>
2022-01-05 16:33:36 +0900

    캐시2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 const data = require('../data/test1.json')
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express release1233312312' });
+  res.render('index', { title: 'Express releas' });
 });
 router.get('/1', function(req, res, next) {
   console.log(typeof(data))
