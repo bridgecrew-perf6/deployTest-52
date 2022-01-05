f4fd1f06db112147956214294152f0a3c8d88337
wls0 <kjy0836@naver.com>
2022-01-05 12:13:19 +0900

    5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express re3' });
+  res.render('index', { title: 'Express release123' });
 });
 
 module.exports = router;
